dc36c9aae9e50ca9d1b7b8796665b55ca56c3f83
David Goldrock <gaming131213@gmail.com>
2024-06-19 19:26:51 +0300

    First commit: created firefox version
    Signed-off-by: David Goldrock <gaming131213@gmail.com>

---
+++ FireFox/TumblrHebrewCursiveExtension.js
@@ -0,0 +1,4 @@
+var elements = document.querySelectorAll('.k31gt .G6Sn4');
+for(var i=0; i<elements.length; i++){
+    elements[i].style.fontFamily  = "Fairwater, Guttman Yad, Georgia,Times,Times New Roman,serif";
+}
